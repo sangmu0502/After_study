317daaa0ef7a9d763a754c092a07d9ab78f22dc0
sangmu <dltkdandl@naver.com>
2024-02-13 15:48:13 +0900

    after_study

---
+++ After_study_list3/012.sum.py
@@ -0,0 +1,11 @@
+"""
+다음 100X100의 2차원 배열이 주어질 때, 각 행의 합, 각 열의 합, 각 대각선의 합 중 최댓값을 구하는 프로그램을 작성하여라.
+다음과 같은 5X5 배열에서 최댓값은 29이다.
+[제약 사항]
+총 10개의 테스트 케이스가 주어진다.
+배열의 크기는 100X100으로 동일하다.
+각 행의 합은 integer 범위를 넘어가지 않는다.
+동일한 최댓값이 있을 경우, 하나의 값만 출력한다
+
+testcase = https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AY1S2R462h4DFAWX&contestProbId=AV13_BWKACUCFAYh&probBoxId=AY1andpK_vQDFAWX&type=PROBLEM&problemBoxTitle=240131_List2_1&problemBoxCnt=4 참초
+"""
